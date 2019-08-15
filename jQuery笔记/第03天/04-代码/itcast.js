dea2545d3b53af9a7c8bfb0a21189716c5beeba5
naxinyi <780957235@qq.com>
2019-08-15 11:13:06 +0800

    include.php

--- jQuery笔记/第03天/04-代码/itcast.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by HUCC on 2017/4/11.
- */
-var $ = {
-  name:"zs",
-  age:18
-};
--- jQuery笔记/第04天/04-代码/jquery.bgColor.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by HUCC on 2017/4/12.
- */
-$.fn.bgColor = function (color) {
-  //this是一个jq对象
-  this.css("backgroundColor", color);
-
-  return this;
-};
