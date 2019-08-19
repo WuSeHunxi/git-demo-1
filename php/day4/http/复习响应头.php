d7c7cdff926ff0cca74f64c0c0ed950cefd7cfb5
naxinyi <780957235@qq.com>
2019-08-19 09:06:35 +0800

    8.19

---
+++ php/day4/http/复习响应头.php
@@ -0,0 +1,25 @@
+<?php
+
+
+//设置响应文件类型
+// header('Content-Type:text/css;charset:UTF-8');
+
+//下载  满足下载条件
+// header('Content-Type:index/css;charset:UTF-8');
+
+//重定向跳转
+header('Location:text.css');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>哈哈哈</h1>
+</body>
+</html>
