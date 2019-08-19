805adb25915a177c1c62049662ada9655fa5b97f
naxinyi <780957235@qq.com>
2019-08-19 08:20:00 +0800

    8.19

--- php/day4/http/数据表述手段.php
+++ php/day4/http/数据表述手段.php
@@ -1,2 +1,11 @@
 <?php
 
+//字面量用于描述一个有结构的数据，是代码中表述数据的手段.
+[
+    {id:1,name:"ll",email:'aaa@.com',url:'ajjaj'},
+    {id:1,name:"ll",email:'aaa@.com',url:'ajjaj'},
+    {id:1,name:"ll",email:'aaa@.com',url:'ajjaj'}
+]
+
+//JSON 是一种类似于js的字面量手段，是数据的表述手段，也是现如今用的最多的数据格式。
+
