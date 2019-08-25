748e829b3f9ab804036053977b2940c274fd80e5
naxinyi <780957235@qq.com>
2019-08-25 21:32:11 +0800

    8.25

--- php/day5/数据库中char和varchar的区别.php
+++ php/day5/数据库中char和varchar的区别.php
@@ -4,6 +4,15 @@
 //  varchar 是可变长度
 //  select * from 表名; //查找语句
 
-//  修改
+//  修改  可以有条件(where)；并列用and
 //  update 表名 set xxx=xx; （字段名=值）
+//  update users ste title='CEO' where id=1;
+
+
+//  常见的查询函数
+//  count max min avg
+//  select count(1) as count from users; 求数据条数和
+//  limit 分页限制
+//  select * from users limit 2; 限制了取两条
+
 
