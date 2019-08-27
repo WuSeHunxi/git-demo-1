64987c5fd5990b82935248e5796f7b9743b20dc8
naxinyi <780957235@qq.com>
2019-08-27 12:05:26 +0800

    8.27

---
+++ php/day6/复习/list.php
@@ -0,0 +1,54 @@
+<?php
+
+// 动态显示数据
+
+$json=file_get_contents('data.json');
+
+// 解码得到一个关联数组
+$data=json_decode($json,true);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>音乐列表</title>
+  <link rel="stylesheet" href="bootstrap.css">
+</head>
+<body>
+  <div class="container py-5">
+    <h1 class="display-4">音乐列表</h1>
+    <hr>
+    <div class="mb-3">
+      <a href="add.php" class="btn btn-secondary btn-sm">添加</a>
+    </div>
+    <table class="table table-bordered table-striped table-hover">
+      <thead class="thead-dark">
+        <tr>
+          <th class="text-center">标题</th>
+          <th class="text-center">歌手</th>
+          <th class="text-center">海报</th>
+          <th class="text-center">音乐</th>
+          <th class="text-center">操作</th>
+        </tr>
+      </thead>
+      <tbody class="text-center">
+        <?php foreach ($data as $item): ?>
+        <tr>
+          <td class="align-middle"><?php echo $item['title']; ?></td>
+          <td class="align-middle"><?php echo $item['artist']; ?></td>
+          <td class="align-middle">
+            <?php foreach ($item['images'] as $src): ?>
+            <img src="<?php echo $src; ?>" alt="">
+            <?php endforeach ?>
+          </td>
+          <td class="align-middle"><audio src="<?php echo $item['source']; ?>" controls></audio></td>
+          <td class="align-middle"><a class="btn btn-danger btn-sm" href="delete.php?id=<?php echo $item['id']; ?>">删除</a></td>
+        </tr>
+        <?php endforeach ?>
+      </tbody>
+    </table>
+  </div>
+</body>
+</html>
