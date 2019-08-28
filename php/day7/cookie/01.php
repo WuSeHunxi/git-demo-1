5868cac3cd2c13d44e07bc66d8207d30e1ed60ac
naxinyi <780957235@qq.com>
2019-08-28 18:35:32 +0800

    8.28

---
+++ php/day7/cookie/01.php
@@ -0,0 +1,18 @@
+<?php
+
+// http的一个特点：无状态
+
+// 一个参数是删除cookie
+
+// 两个参数是设置cookie
+setcookie('key','value');
+
+// 三个参数是设置时间
+setcookie('key2','value2',time()+1 * 24 * 60 * 60);
+
+// 七个参数的cookie
+setcookie('ket3','values',time()+1*24*60*60,'','',false,true); // 表示只能通过http访问
+
+setcookie('ket3','values',time()+1*24*60*60,'','',false,false); // 即可以通过http进行访问也可以使用js访问
+
+var_dump($_COOKIE);
