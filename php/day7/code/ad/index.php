26f56446f2c55a24af3020c62a5060e71a55321e
naxinyi <780957235@qq.com>
2019-08-29 18:45:52 +0800

    8.29

--- php/day7/code/ad/index.php
+++ php/day7/code/ad/index.php
@@ -4,7 +4,7 @@
 // 在PHP中判断是否设置了这个action参数
 if (isset($_GET['action']) && $_GET['action'] === 'close-ad') {
   // 不想看到广告
-  setcookie('hide_ad', '1');
+  setcookie('hide_ad', '1'); // 此时只是设置了cookie，但是没有取出cookie的值，在HTML中需要用到，因此需要下面那行代码
   $_COOKIE['hide_ad'] === '1'; // 这行必须得有，当设置完cookie之后
 }
 
