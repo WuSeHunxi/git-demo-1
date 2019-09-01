c68d747d7eeab01d46cd831a314576d1a89339ee
naxinyi <780957235@qq.com>
2019-09-01 23:10:44 +0800

    9.1

--- php/day5/song/list1.php
+++ php/day5/song/list1.php
@@ -12,6 +12,26 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container py-5">
+        <h1 class="display-4">音乐列表</h1>
+        <hr>
+        <div class="mb-3">
+            <a href="add.php" class="btn btn-secondary btn-sm">添加</a>
+        </div>
+        <table class="table table-bordered table-striped table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th class="text-center">标题</th>
+                    <th class="text-center">歌手</th>
+                    <th class="text-center">海报</th>
+                    <th class="text-center">音乐</th>
+                    <th class="text-center">操作</th>
+                </tr>
+                <thead class="text-center">
+                    
+                </thead>
+            </thead>
+        </table>
+    </div>
 </body>
 </html>
