16f885f76bcfc508d02af5186cb5af85d5e7b01c
naxinyi <780957235@qq.com>
2019-09-01 13:31:11 +0800

    9.1

--- Ajax/案例/login.php
+++ Ajax/案例/login.php
@@ -1,2 +1,11 @@
 <?php
 
+if(empty($_POST["name"])||empty($_POST["password"])){
+    exit("请输入用户名和密码");
+}
+
+if($_POST["name"]=="haha"&&$_POST["password"]==123){
+   exit("登陆成功");
+}else{
+    exit("登陆失败");
+}
