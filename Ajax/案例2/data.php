6363d046f800dee6677972ae2f61d08dbe60f199
naxinyi <780957235@qq.com>
2019-09-02 20:56:08 +0800

    9.2

--- Ajax/案例2/data.php
+++ Ajax/案例2/data.php
@@ -1,2 +1,24 @@
 <?php
 
+$data=array(
+    array("name"=>"haha","id"=>11,"age":141),
+    array("name"=>"xiaoxiao","id"=>12,"age":115),
+    array("name"=>"lala","id"=>13,"age":116),
+    array("name"=>"mumu","id"=>14,"age":117)
+);
+
+if(empty($_GET["REQUEST_METHOD"])){
+
+}
+
+if(empty($_GET['id'])){
+    $json=json_encode($data);
+    echo $json;
+}else{
+    foreach ($data as $item) {
+        if($_GET['id']==$item['id']){
+            $json=json_encode($item);
+            echo $json;
+        }
+    }
+}
