35925dec8b7ae0667c1592ad6c473c6f9fe5dcc0
naxinyi <780957235@qq.com>
2019-09-04 15:41:20 +0800

    9.4

--- php/复习/02-表单.php
+++ php/复习/02-表单.php
@@ -1,6 +1,31 @@
 <?php
 
+function login_success(){
+    // 开始检验
+    if(empty($_POST['name'])){
+        $GLOBALS['message']='请输入用户名';
+        return;
+    }
+    if(empty($_POST['password'])){
+        $GLOBALS['message']='请输入密码';
+        return;
+    }
+    if(empty($_POST['confirm'])){
+        $GLOBALS['message']='请输入确认密码';
+        retrun;
+    }
+    if($_POST['passqord']!==$_POST['confirm']){
+        $GLOBALS['message']='两次输入的密码不一致';
+        rteurn;
+    }
+    
+    $name=$_POST['name'];
+    $password=$_POST['password'];
+}
 
+if($_SERVER['REQUEST_METHOD']==='POST'){
+    login_success();
+}
 
 ?>
 <!DOCTYPE html>
@@ -12,7 +37,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     <table>
         <tr>
             <td>用户名</td>
@@ -24,7 +49,7 @@
         </tr>
         <tr>
             <td>确认密码</td>
-            <td><input type="password" name="password" id="password"></td>
+            <td><input type="password" name="confirm" id="confirm"></td>
         </tr>
         <tr>
             <td><button>登录</button></td>
