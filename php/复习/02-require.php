bd852db7dc35c8744a01a0719f5d7d57028baedd
naxinyi <780957235@qq.com>
2019-09-04 16:06:41 +0800

    9.4

--- php/复习/02-require.php
+++ php/复习/02-require.php
@@ -1,3 +1,6 @@
 <?php
 
 
+$config = require 'config.php'; //当文件载入的时候使用了return
+
+var_dump($config);
--- php/复习/config.php
+++ php/复习/config.php
@@ -1,5 +1,10 @@
 <?php
 
-$array=(
-    'SEVER_MES'=>'lallal';
-)
+return array(
+    'SYSTEM_MES'=>''  // 注意语法错误，后面没有符号
+);
+
+// return array(
+//     'SYSTEM_NAME' => ''
+//   );  //关联数组中的值是常量为空字符串
+  
