3db4cef46b4ea9d68496e13b80416dc2149c4e56
naxinyi <780957235@qq.com>
2019-09-04 18:27:31 +0800

    9.4

---
+++ Ajax/复习/user.php
@@ -0,0 +1,25 @@
+<?php
+
+$array=(
+    $array=(
+        "name"=>"lalal",
+        "id"=>1,
+        "age"=>16
+    ),
+    $array=(
+        "name"=>"lalal",
+        "id"=>1,
+        "age"=>18
+    ),
+    $array=(
+        "name"=>"lalal",
+        "id"=>3,
+        "age"=>17
+    )
+    $array=(
+        "name"=>"lalal",
+        "id"=>4,
+        "age"=>19
+    )
+);
+
