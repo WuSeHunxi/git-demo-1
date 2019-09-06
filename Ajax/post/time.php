8371befd3fbe5d090607bfddea22ee2debb43479
naxinyi <780957235@qq.com>
2019-09-07 06:24:19 +0800

    9.7

--- Ajax/post/time.php
+++ Ajax/post/time.php
@@ -1,19 +1,28 @@
+
 <?php
+$username=$_POST["name"];
+$password=$_POST["password"];
+echo "用户名: ".$username;
+echo "<br>";
+echo "密码: ".$password;
 
 
-function login(){
-    if(empty($_GET['name'])){
-        $GLOBALS['message']="请输入姓名";
-        return;
-    }
-    if(empty($_GET['possword'])){
-        $GLOBALS['message']='请输入密码';
-        return;
-    }
-    $name=$_GTE['name'];
-    $password=$_GET['password'];
-}
 
-if($_SERVER['REQUEST_METHOD']==='GET'){
-    login();
-}
+// function login(){
+//     if(empty($_POST['name'])){
+//         $GLOBALS['message']="请输入姓名";
+//         return;
+//     }
+//     if(empty($_POST['possword'])){
+//         $GLOBALS['message']='请输入密码';
+//         return;
+//     }
+//     $name=$_POST['name'];
+//     $password=$_POST['password'];
+// }
+
+// if($_SERVER['REQUEST_METHOD']==='POST'){
+//     login();
+// }
+
+?>
