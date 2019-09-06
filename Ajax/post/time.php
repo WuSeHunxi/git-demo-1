32ec729fb0626b089f852365711ad461f0013e33
naxinyi <780957235@qq.com>
2019-09-06 20:51:51 +0800

    9.6

--- Ajax/post/time.php
+++ Ajax/post/time.php
@@ -1,2 +1,19 @@
 <?php
-echo time();
+
+
+function login(){
+    if(empty($_GET['name'])){
+        $GLOBALS['message']="请输入姓名";
+        return;
+    }
+    if(empty($_GET['possword'])){
+        $GLOBALS['message']='请输入密码';
+        return;
+    }
+    $name=$_GTE['name'];
+    $password=$_GET['password'];
+}
+
+if($_SERVER['REQUEST_METHOD']==='GET'){
+    login();
+}
