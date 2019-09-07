a33e01511f060337c046d066dc25411e968b51f9
naxinyi <780957235@qq.com>
2019-09-07 22:51:35 +0800

    9.7

--- Ajax/post/复习/login.php
+++ Ajax/post/复习/login.php
@@ -1,25 +1,28 @@
 <?php
-
-$id=array();
-$id['1000']=arrat(
-    "info"=>"小王的快递",
-    "status"=>"配送中"
-);
-
-$id['1001']=arrat(
-    "info"=>"小孙的快递",
-    "status"=>"配送中"
-);
-
-$id['1002']=arrat(
-    "info"=>"小李的快递",
-    "status"=>"已配送"
-);
-
-if(!empty($_GET['id'])){
-    if(empty($_GET['callback'])){
-        echo json_encode($id[$_GET['id']]);
-    }else{
-        echo $_GET['callback']."(".json_encode($id[$_GET['id']]).")";
+    $kuaidi = array();
+    $kuaidi["1000"] = array(
+        "info" => '王小三的快递',
+        'status' => '配送中', 
+    );
+    $kuaidi["1001"] = array(
+        "info" => '王三的快递',
+        'status' => '配送中', 
+    );
+    $kuaidi["1002"] = array(
+        "info" => '张三的快递',
+        'status' => '已配送', 
+    );
+    /*    * 传送方式 GET
+         * 参数 kuaidi_id 需要查询的快递单号
+         * callback 回调函数名
+     * */
+    if(!empty($_GET["kuaidi_id"])){
+        //如果没有传回调函数
+        if(empty($_GET["callback"])){
+            echo json_encode($kuaidi[$_GET["kuaidi_id"]]);
+        }else{
+            //如果有回调函数 那么输出 jsonp
+            echo $_GET["callback"]."(".json_encode($kuaidi[$_GET["kuaidi_id"]]).")";
+        }
     }
-}
+?>
