fe2d2bb9a52009675ccd50dd607e8b1ede8bd98d
naxinyi <780957235@qq.com>
2019-09-07 15:07:22 +0800

    9.7

--- Ajax/post/user.php
+++ Ajax/post/user.php
@@ -1,9 +1,15 @@
 <?php
 
-$name=$_POST['name'];
-$password=$_POST['password'];
 
-echo "Username".$name;
-echo "password".$password;
+$name="lalala";
+$password=$_POST['password'];
+if($_POST['name']===$name&&$_POST['password']===$password){
+    echo "登陆成功";
+}else{
+    echo "登陆失败";
+}
+
+// echo "Username".$name;
+// echo "password".$password;
 
 ?>
