5ecc08c3eb143482d6dd504b883aae33b86c317a
naxinyi <780957235@qq.com>
2019-09-10 20:12:56 +0800

    9.10

--- Ajax/post/json.php
+++ Ajax/post/json.php
@@ -6,25 +6,13 @@ $zhangsan = array(
     array('id'=>3,'name' => 'ahahahah', 'age' => 14)
 );
 
-// $array=(
-//     array(
-//         "name"=>"lalal",
-//         "id"=>1,
-//         "age"=>18
-//     ),
-//     array(
-//         "name"=>"hhhh",
-//         "id"=>2,
-//         "age"=>14
-//     ),
-//     array(
-//         "name"=>"mamama",
-//         "id"=>2,
-//         "age"=>10
-//     )
-//     );
 
-// 于情于理都应该设置 application/json
-// header('Content-Type: application/json');
+foreach ($zhangsan as $key) {
+    foreach ($key as $col) {
+        if($_GET['id']==2){
+            $data=json_encode($key);
+        }
+    }
+}
 
-echo json_encode($zhangsan);
+// echo json_encode($zhangsan);
